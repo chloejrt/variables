657ca1282eb67458eb5abf5cd6e1151dd6c5d6ad
chloejrt <145430963+chloejrt@users.noreply.github.com>
2023-09-22 11:43:53 +0200

    Add files via upload

---
+++ variables/github/exo.js
@@ -0,0 +1,20 @@
+"use strict"
+
+let nombreAvengers=4 // number
+let nomdefamilledelempereurromainJules= "Cesar" // string
+let nombreetoileunivers= 100 //number
+let messagePaillasson= "bienvenue" // string
+let presenceFaceid= true // boolean
+let prixOrdi= 1000 // number
+let nomsseptnains= "il y a prof grincheux, atchoum, dormeur, simplet, joyeux, timide" // string ou tableau = type objet
+let pseudoBg= "bg 78" // string
+let clonesNaruto = 8 // number
+let troisDefauts= " impatient, perfectionniste, bavard" // string ou tableau = type objet
+let prenomAnne= "Anne" // string
+let pannierAmazon= "ecouteurs, casque"// string
+let guerre100ans= 100 // number
+let caracteristiquesBierre= "pas bon" // string ou tableau = type objet
+let nomprochainPresident= "cloclo"// string
+
+const // variable non modifiable
+pseudo = "cloclo"
